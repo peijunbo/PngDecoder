6196fa85420c24c8e9b9978c09b1dd793a324111
peijunbo--list <391745259@qq.com>
2021-09-28 10:03:03 +0800

    feat

--- code/PngDecoder.py
+++ code/PngDecoder.py
@@ -6,58 +6,58 @@ import os
 
 # 创建数据块类每个类变量内有该数据块的信息
 class Chunk:
-    def __init__(self, index, bytesls, list):
+    def __init__(self, index, bytesls, intlist):
         self.bLength = bytesls[index:index+4]
-        self.Length = list[index]*(16**6) + list[index+1]*(16**4) + list[index + 2]*(16**2) + list[index + 3]
+        self.Length = intlist[index]*(16**6) + intlist[index+1]*(16**4) + intlist[index + 2]*(16**2) + intlist[index + 3]
         index += 4
         self.bType = bytesls[index:index + 4]
-        self.Type = chr(list[index]) + chr(list[index+1]) + chr(list[index+2]) + chr(list[index+3])
+        self.Type = chr(intlist[index]) + chr(intlist[index+1]) + chr(intlist[index+2]) + chr(intlist[index+3])
         index += 4
 
         # 处理数据块
         self.Dataindex = index
 
-        self.Data = list[index:index+self.Length]
-        self.bData = bytesls[index:index+self.Length]
+        self.Data = intlist[index:index+self.Length]
+        self.bData = list(bytesls[index:index+self.Length])
         index += self.Length
         # CRC
         self.bCRC = bytesls[index:index + 4]
-        self.CRC = [list[index], list[index+1], list[index+2], list[index+3]]
+        # self.CRC = [list[index], list[index+1], list[index+2], list[index+3]]
         self.endindex = index + 4
 
-    def IHDRWidth(self, list):
+    def IHDRWidth(self, intlist):
         if self.Type == "IHDR":
-            Width = list[self.Dataindex]*(16**6) + list[self.Dataindex+1]*(16**4) + list[self.Dataindex + 2]*(16**2) + list[self.Dataindex + 3]
+            Width = intlist[self.Dataindex]*(16**6) + intlist[self.Dataindex+1]*(16**4) + intlist[self.Dataindex + 2]*(16**2) + intlist[self.Dataindex + 3]
             return Width
 
-    def IHDRHeight(self, list):
+    def IHDRHeight(self, intlist):
         if self.Type == "IHDR":
-            Height = list[self.Dataindex+4]*(16**6) + list[self.Dataindex+5]*(16**4) + list[self.Dataindex + 6]*(16**2) + list[self.Dataindex + 7]
+            Height = intlist[self.Dataindex+4]*(16**6) + intlist[self.Dataindex+5]*(16**4) + intlist[self.Dataindex + 6]*(16**2) + intlist[self.Dataindex + 7]
             return Height
 
-    def IHDRBitDepth(self, list):
+    def IHDRBitDepth(self, intlist):
         if self.Type == "IHDR":
-            BitDepth = list[self.Dataindex + 8]
+            BitDepth = intlist[self.Dataindex + 8]
             return BitDepth
 
-    def IHDRColorType(self, list):
+    def IHDRColorType(self, intlist):
         if self.Type == "IHDR":
-            ColorType = list[self.Dataindex + 9]
+            ColorType = intlist[self.Dataindex + 9]
             return ColorType
 
-    def IHDRComMethod(self, list):
+    def IHDRComMethod(self, intlist):
         if self.Type == "IHDR":
-            ComMethod = list[self.Dataindex + 10]
+            ComMethod = intlist[self.Dataindex + 10]
             return ComMethod
 
-    def IHDRFilMethod(self, list):
+    def IHDRFilMethod(self, intlist):
         if self.Type == "IHDR":
-            FilMethod = list[self.Dataindex + 11]
+            FilMethod = intlist[self.Dataindex + 11]
             return FilMethod
 
-    def IHDRIntMethod(self, list):
+    def IHDRIntMethod(self, intlist):
         if self.Type == "IHDR":
-            IntMethod = list[self.Dataindex + 12]
+            IntMethod = intlist[self.Dataindex + 12]
             return IntMethod
 
     def reidat(self):
@@ -82,7 +82,7 @@ def bytes_to_int(bytes):
 def setchunk(ls, originls, index=8):
     outlist = []
     while index < len(ls):
-        ck = Chunk(index, originls, list=ls)
+        ck = Chunk(index, originls, intlist=ls)
         outlist.append(ck)
         index = ck.endindex
     return outlist
@@ -161,38 +161,43 @@ class PNG:
         self.ComMethod = self.IHDR.IHDRComMethod(self.intlist)
         self.FilMethod = self.IHDR.IHDRFilMethod(self.intlist)
         self.IntMethod = self.IHDR.IHDRIntMethod(self.intlist)
-
-        # 处理自身的rgb
-        fillist = []
-        decompress = list(self.decompress_idat())
-        for i in range(self.Height):
-            a = decompress.pop(i*self.Width*3)
-            a = bytes_to_int(a)
-            fillist.append(a)
-        self.rlist = []
-        self.glist = []
-        self.blist = []
-        rgblist = []
-        for i in decompress:
-            rgblist.append(int(i.hex(), 16))
-        for i in range(len(rgblist)):
-            a = rgblist[i]
-            if i % 3 == 0:
-                self.rlist.append(a)
-            elif i % 3 == 1:
-                self.glist.append(a)
-            else:
-                self.blist.append(a)
-        self.rlist = getrgb(self, self.rlist, fillist)
-        self.glist = getrgb(self, self.glist, fillist)
-        self.blist = getrgb(self, self.blist, fillist)
-        self.gray = False
+        if self.ColorType == 0:
+            self.gray = True
+        else:
+            self.gray = False
+        if self.gray:
+            pass
+        else:
+            # 处理自身的rgb
+            fillist = []
+            decompress = list(self.decompress_idat())
+            for i in range(self.Height):
+                a = decompress.pop(i*self.Width*3)
+                a = bytes_to_int(a)
+                fillist.append(a)
+            self.rlist = []
+            self.glist = []
+            self.blist = []
+            rgblist = []
+            for i in decompress:
+                rgblist.append(int(i.hex(), 16))
+            for i in range(len(rgblist)):
+                a = rgblist[i]
+                if i % 3 == 0:
+                    self.rlist.append(a)
+                elif i % 3 == 1:
+                    self.glist.append(a)
+                else:
+                    self.blist.append(a)
+            self.rlist = getrgb(self, self.rlist, fillist)
+            self.glist = getrgb(self, self.glist, fillist)
+            self.blist = getrgb(self, self.blist, fillist)
 
     def decompress_idat(self):
         outlist = []
         for i in self.cklist:
             if i.Type == "IDAT":
-                outlist.extend(self.byteslist[i.Dataindex:i.Dataindex+i.Length])
+                outlist.extend(i.bData)
         outlist = struct.pack(str(len(outlist))+"c", *outlist)
         outlist = zlib.decompress(outlist)
         outlist = struct.unpack(str(len(outlist))+"c", outlist)
@@ -230,6 +235,40 @@ class PNG:
         for i in range(len(insertlist)):
             self.cklist.insert(insertindex, insertlist[i])
 
+    def modify_graypng(self, graylist):
+        self.graylist = graylist
+        idat = []
+        filter0 = 0
+        insertindex = 0
+        for i in range(len(graylist)):
+            if i % self.Width == 0:
+                idat.append(filter0.to_bytes(1, "big", signed=False))
+            idat.append(graylist[i].to_bytes(1, "big", signed=False))
+        idat = struct.pack(str(len(idat)) + 'c', *idat)
+        idat = zlib.compress(idat)
+        idat = struct.unpack(str(len(idat)) + 'c', idat)
+        for i in range(len(self.cklist)):
+            if self.cklist[i].Type == "IDAT":
+                insertindex = i
+                break
+        for i in self.cklist:
+            if i.Type == "IDAT":
+                del i
+        insertlist = createidat(idat)
+        for i in range(len(insertlist)):
+            self.cklist.insert(insertindex, insertlist[i])
+        if self.gray:
+            pass
+        else:
+            self.cklist[0].Data[9] = 0
+            self.cklist[0].bData[9] = self.cklist[0].Data[9].to_bytes(1, "big", signed=False)
+            self.cklist[0].bCRC = list(struct.unpack('4c',  zlib.crc32(
+                struct.pack('17c', *self.cklist[0].bType, *self.cklist[0].bData)).to_bytes(4, 'big', signed=False)))
+            self.gray = True
+            del self.rlist
+            del self.glist
+            del self.blist
+
     def repack(self):
         packls = []
         packls.extend(self.pnghead)
@@ -242,11 +281,16 @@ class PNG:
         return outpack
 
     def changegray(self):
+        # for i in range(len(self.rlist)):
+        #     gray = (self.rlist[i] * 30 + self.glist[i] * 59 + self.blist[i] * 11) // 100
+        #
+        #     self.rlist[i] = self.glist[i] = self.blist[i] = gray
+        # self.modify_png(self.rlist, self.glist, self.blist)
+        # self.gray = True
+        graylist = []
         for i in range(len(self.rlist)):
-            gray = (self.rlist[i] * 30 + self.glist[i] * 59 + self.blist[i] * 11) // 100
-            self.rlist[i] = self.glist[i] = self.blist[i] = gray
-        self.modify_png(self.rlist, self.glist, self.blist)
-        self.gray = True
+            graylist.append((self.rlist[i] * 30 + self.glist[i] * 59 + self.blist[i] * 11) // 100)
+        self.modify_graypng(graylist)
 
     def getchpainting(self, step=10):
         if not self.gray:
@@ -277,31 +321,39 @@ class PNG:
         if not self.gray:
             self.changegray()
         graylist = []
-        graylist.extend(self.rlist)
+        graylist.extend(self.graylist)
         for x in range(1, self.Height-1):
             for y in range(1, self.Width-1):
                 # b c d
                 # e a f
                 # g h i
                 index = self.xy_to_rgbindex(x, y)
-                # a = graylist[self.xy_to_rgbindex(x, y)]
-                b = graylist[self.xy_to_rgbindex(x-1, y-1)]
-                c = graylist[self.xy_to_rgbindex(x-1, y)]
-                d = graylist[self.xy_to_rgbindex(x-1, y+1)]
-                e = graylist[self.xy_to_rgbindex(x, y-1)]
-                f = graylist[self.xy_to_rgbindex(x, y+1)]
-                g = graylist[self.xy_to_rgbindex(x+1, y-1)]
-                h = graylist[self.xy_to_rgbindex(x+1, y)]
-                i = graylist[self.xy_to_rgbindex(x+1, y+1)]
+                a = self.xy_to_rgbindex(x, y)
+                # b = graylist[self.xy_to_rgbindex(x-1, y-1)]
+                # c = graylist[self.xy_to_rgbindex(x-1, y)]
+                # d = graylist[self.xy_to_rgbindex(x-1, y+1)]
+                # e = graylist[self.xy_to_rgbindex(x, y-1)]
+                # f = graylist[self.xy_to_rgbindex(x, y+1)]
+                # g = graylist[self.xy_to_rgbindex(x+1, y-1)]
+                # h = graylist[self.xy_to_rgbindex(x+1, y)]
+                # i = graylist[self.xy_to_rgbindex(x+1, y+1)]
+                b = graylist[a-self.Width-1]
+                c = graylist[a-self.Width]
+                d = graylist[a-self.Width+1]
+                e = graylist[a-1]
+                f = graylist[a+1]
+                g = graylist[a+self.Width-1]
+                h = graylist[a+self.Width]
+                i = graylist[a+self.Width+1]
                 fx = abs((2 * f + d + i - 2 * e - b - g)) // 4
                 fy = abs((2 * h + g + i - 2 * c - b - d)) // 4
                 gradient = int((fx ** 2 + fy ** 2) ** 0.5)
                 if gradient > 255:
                     gradient = 255
                 gradient = 255 - gradient
-                self.rlist[index] = self.glist[index] = self.blist[index] = gradient
+                self.graylist[index] = gradient
 
-        self.modify_png(self.rlist, self.glist, self.blist)
+        self.modify_graypng(self.graylist)
 
     def createpng(self, pngname='test.png'):
         pngfile = open(pngname, mode='wb')
--- code/main.py
+++ code/main.py
@@ -1,3 +1,12 @@
+import time
 import PngDecoder as PD
+start = time.perf_counter()
+# 图片地址
+Paddress1 = "C:\\Users\\裴俊博\\Desktop\\实习题-实现png解码器\\后端组实习题PngDecoder\\task3\\test1.png"
 
-PD.create_video('..\\test\\video.mp4', '..\\test\\testvideo.mp4')
+# 处理图片
+PNGpicture = PD.PNG(Paddress1)
+PNGpicture.changegray()
+PNGpicture.createpng()
+end = time.perf_counter()
+print(end-start)
